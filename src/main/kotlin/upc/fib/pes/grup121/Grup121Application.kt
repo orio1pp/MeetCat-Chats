84aa914034d39957deecf97b722626aeaceef619
orio1pp <oriol.portell@estudiantat.upc.edu>
2022-10-14 15:10:21 +0200

    Setup

---
+++ src/main/kotlin/upc/fib/pes/grup121/Grup121Application.kt
@@ -0,0 +1,11 @@
+package upc.fib.pes.grup121
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Grup121Application
+
+fun main(args: Array<String>) {
+	runApplication<Grup121Application>(*args)
+}
---
+++ src/test/kotlin/upc/fib/pes/grup121/Grup121ApplicationTests.kt
@@ -0,0 +1,13 @@
+package upc.fib.pes.grup121
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Grup121ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
